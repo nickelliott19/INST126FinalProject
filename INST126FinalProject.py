21cdeaf54fd9b9e67e3f0b01584c7ada196da13e
Nicholas Elliott <nelliot2@terpmail.umd.edu>
2023-12-19 21:07:06 -0500

    Added Comments

--- INST126FinalProject.py
+++ INST126FinalProject.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 def get_github_repo_info(nickelliott19, JOUR472_fall2023):
-
+    # Getting repository info from GitHub API
     api_url = f"https://api.github.com/repos/{nickelliott19}/{JOUR472_fall2023}"
     response = requests.get(api_url)
 
@@ -15,14 +15,14 @@ def get_github_repo_info(nickelliott19, JOUR472_fall2023):
         return None
 
 def extract_commit_activity(nickelliott19, JOUR472_fall2023):
-
+    # Getting commit activity using BeautifulSoup
     commit_activity_url = f"https://github.com/{nickelliott19}/{JOUR472_fall2023}/graphs/commit-activity"
     response = requests.get(commit_activity_url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
-
+        # Getting the commit activity info using regular expressions
         commit_activity_text = soup.find("a", class_="js-selected-navigation-item.selected").text
         commit_activity = re.findall(r'(\d+)\s+contributions\s+in the last year', commit_activity_text)
         return commit_activity[0] if commit_activity else None
@@ -31,11 +31,11 @@ def extract_commit_activity(nickelliott19, JOUR472_fall2023):
         return None
 
 def main():
-
+    # Github Repo Info
     github_username = "nickelliott19"
     github_repo = "JOUR472_fall2023"
 
-
+    # Getting repo info from GitHub API
     repo_info = get_github_repo_info(github_username, github_repo)
 
     if repo_info:
@@ -49,7 +49,7 @@ def main():
         print(f"Created at: {repo_info['created_at']}")
         print(f"Last pushed at: {repo_info['pushed_at']}")
 
-   
+        # Getting and printing the commit activity
         commit_activity = extract_commit_activity(github_username, github_repo)
         if commit_activity:
             print(f"\nCommit Activity in the Last Year: {commit_activity} contributions")
