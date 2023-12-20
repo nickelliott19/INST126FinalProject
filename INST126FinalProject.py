de5a5d9191320bf918a5b05a3accf839952dc640
Nicholas Elliott <nelliot2@terpmail.umd.edu>
2023-12-19 22:15:19 -0500

    Final Commit

--- INST126FinalProject.py
+++ INST126FinalProject.py
@@ -1,43 +1,37 @@
 import requests
-from bs4 import BeautifulSoup
 import re
 
-def get_github_repo_info(nickelliott19, JOUR472_fall2023):
+def get_github_repo_info(username, repo_name):
     # Getting repository info from GitHub API
-    api_url = f"https://api.github.com/repos/{nickelliott19}/{JOUR472_fall2023}"
+    api_url = f"https://api.github.com/repos/{username}/{repo_name}"
     response = requests.get(api_url)
 
+    # Checking if the HTTP status code is 200 and if so, it's successful
     if response.status_code == 200:
+
+        # If successful, this extracting the JSON content and analyzing it
         repo_info = response.json()
         return repo_info
+    
+    # If the HTTP status code isnt 200, then they it prints out an error message with the status code
     else:
         print(f"Error: Unable to fetch repository information. Status Code: {response.status_code}")
         return None
-
-def extract_commit_activity(nickelliott19, JOUR472_fall2023):
-    # Getting commit activity using BeautifulSoup
-    commit_activity_url = f"https://github.com/{nickelliott19}/{JOUR472_fall2023}/graphs/commit-activity"
-    response = requests.get(commit_activity_url)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Getting the commit activity info using regular expressions
-        commit_activity_text = soup.find("a", class_="js-selected-navigation-item.selected").text
-        commit_activity = re.findall(r'(\d+)\s+contributions\s+in the last year', commit_activity_text)
-        return commit_activity[0] if commit_activity else None
-    else:
-        print(f"Error: Unable to fetch commit activity. Status Code: {response.status_code}")
-        return None
+    
+def extract_year_month(timestamp):
+    # Using the regular expression "re.search" to change the "Created at" and "Last pushed at" to a cleaner output of dates
+    match = re.search(r'(\d{4}-\d{2})', timestamp)
+    return match.group() if match else None
 
 def main():
-    # Github Repo Info
-    github_username = "nickelliott19"
-    github_repo = "JOUR472_fall2023"
+    # Getting the user to input a GitHub username and respository so they can see the details of anyone they want
+    github_username = input("Enter GitHub username: ")
+    github_repo = input("Enter GitHub repository: ")
 
     # Getting repo info from GitHub API
     repo_info = get_github_repo_info(github_username, github_repo)
 
+    # Printing out all the details on a GitHub profile like the name of it or whether people have shown appreciation (stars)
     if repo_info:
         print(f"GitHub Repository Information for {github_username}/{github_repo}:")
         print(f"Name: {repo_info['name']}")
@@ -46,17 +40,9 @@ def main():
         print(f"Forks: {repo_info['forks_count']}")
         print(f"Watchers: {repo_info['subscribers_count']}")
         print(f"Language: {repo_info['language']}")
-        print(f"Created at: {repo_info['created_at']}")
-        print(f"Last pushed at: {repo_info['pushed_at']}")
+        print(f"Created at: {extract_year_month(repo_info['created_at'])}")
+        print(f"Last pushed at: {extract_year_month(repo_info['pushed_at'])}")
 
-        # Getting and printing the commit activity
-        commit_activity = extract_commit_activity(github_username, github_repo)
-        if commit_activity:
-            print(f"\nCommit Activity in the Last Year: {commit_activity} contributions")
-        else:
-            print("Commit activity information not available.")
-    else:
-        print("Exiting due to an error.")
-
+# This is used to show that the script is run as the main program because it's being executed directly
 if __name__ == "__main__":
     main()
